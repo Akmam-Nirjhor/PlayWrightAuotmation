aee594f35211a1672d028bee3edbe34123428380
Akmam Nirjhor <akmamnirjhor47@gmail.com>
2025-12-26 12:58:59 -0800

    Conftest

--- conftest.py
+++ conftest.py
@@ -1,8 +1,20 @@
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def test_commonBrowser():
     print("This Browser Instance Memory")
     return "Pass"
+
+
+@pytest.fixture(scope= "function")
+def test_afterEffectedBrowseer():
+    print("This is Secondary Browser Instance Memory")
+    yield
+    print("Tear Down Validation")
+
+
+
+
+
 
--- test_Case_02.py
+++ test_Case_02.py
@@ -1,17 +1,14 @@
 
 
-
-
-
-
-def test_numbeThird(test_commonBrowser):
+def test_numbeThird(test_commonBrowser , test_afterEffectedBrowseer):
     print("This is Number One Test Case")
 
 
 
-def test_numberFourth(test_commonBrowser):
+
+def test_numberFourth(test_commonBrowser, test_afterEffectedBrowseer):
     print("This is Number Tow Test Case")
 
 
-def test_numberFifth(test_commonBrowser):
+def test_numberFifth(test_commonBrowser , test_afterEffectedBrowseer):
     print("This is Number Three Test Case")
